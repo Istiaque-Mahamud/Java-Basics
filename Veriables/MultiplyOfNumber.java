f6ec6e783c806c9481e04f02597c74fb03d300a4
Istiaqe-Mahamud <istiaque.mahamud@gmail.com>
2025-05-06 15:04:00 +0600

    Java Practice

---
+++ Veriables/MultiplyOfNumber.java
@@ -0,0 +1,8 @@
+public class MultiplyOfNumber {
+    public static void main(String[] args) {
+        int a=5;
+        int b=5;
+        int multi=a*b;
+        System.out.println("Multiplication:"+multi);
+    }
+}
