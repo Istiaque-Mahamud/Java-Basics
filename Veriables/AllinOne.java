88629ece9797a797eb0379478a806544882c3c59
Istiaqe-Mahamud <istiaque.mahamud@gmail.com>
2025-05-07 22:36:38 +0600

    Java Practice

---
+++ Veriables/AllinOne.java
@@ -0,0 +1,16 @@
+public class AllinOne {
+    public static void main(String[] args) {
+        int a=5;
+        int b=5;
+        int sum= a+b;
+        int multiply= a*b;
+        int divide= a/b;
+        int determination= a-b;
+        int modulus= a%b;
+        System.out.println(sum);
+        System.out.println(multiply);
+        System.out.println(divide);
+        System.out.println(determination);
+        System.out.println(modulus);
+    }
+}
